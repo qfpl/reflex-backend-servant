e261ceb4ea2e7ee1545694e2c7238ece209f04b2
Dave Laing <dave.laing.80@gmail.com>
2018-02-12 13:01:21 +1000

    Bump reflex platform

--- reflex-basic-host.nix
+++ reflex-basic-host.nix
@@ -5,8 +5,8 @@ let
     reflex-basic-host = initialNixpkgs.pkgs.fetchFromGitHub {
       owner = "dalaing";
       repo = "reflex-basic-host";
-      rev = "66428fafc3fb5e9735ceb00bcdeab4c42ea59edc";
-      sha256 = "0i94qzn56kd2mxgxnghrjq5qbq7g5nsjrjfcb1hkfs6kgfmgw1yc";
+      rev = "3ba892320a3fb917aa8772945fab4099b00a8fa4";
+      sha256 = "1lhygzw64gmpyqzy7vy4pfpjhcg2jdb6g04mk1c6mcbhcc21na3x";
     };
   };
 in
